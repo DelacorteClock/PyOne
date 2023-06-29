c5a8fc5562d9c3716d0162802c544843c81fa6fc
DelacorteClock <123141973+DelacorteClock@users.noreply.github.com>
2023-06-29 22:10:12 +0100

    Added First Lesson Content

---
+++ Lesson 1/add.py
@@ -0,0 +1,7 @@
+#Request first integer to add and store it like integer a
+a = int(input('What is the first integer to add?'))
+#Request second integer to add and store it like integer b
+b = int(input('What is the second integer to add?'))
+#Add a and b and store it like integer c
+c = a + b
+print('Sum Is: ',c)
